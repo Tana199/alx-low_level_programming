226c04ffe8f1744a82bb1e4e92f8c8911c4b83a1
Tan199 <yolandahwanamata@gmail.com>
2023-02-24 13:41:21 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/*
+ * file - main.h
+ * authore: Genius Villagers
+ * Desc: Header continues prototypes for all functions
+ * used in 0x04-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
+
